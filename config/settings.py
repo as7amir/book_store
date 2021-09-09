9281274985d831dbca5141a257c139b8313f51c3
Amir <as7amir@gmail.com>
2021-09-09 11:04:50 +0430

    ch12

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,8 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 AUTH_USER_MODEL = 'accounts.CustomUser'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
